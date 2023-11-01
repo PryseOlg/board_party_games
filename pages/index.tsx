465a6301ee320a120430b2d087ccc8218faffe69
Oleg <oleg.kuts.02@mail.ru>
2023-11-02 02:21:03 +0500

    Delete trash

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import { Button, Group } from "@mantine/core";
+import Main from "./main/main";
+
+export default function IndexPage() {
+  return (
+    <Group mt={50} justify="center">
+      <Main/>
+    </Group>
+  );
+}
